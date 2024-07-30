b95d791986068bfadef593b06f53359338d689f5
PratikM-stack <pratikmaral23@gmail.com>
2024-07-30 17:24:51 +0530

    Create Program1.py

---
+++ Patterns./Program1.py
@@ -0,0 +1,10 @@
+row= int(input("enter row :"))
+num=1
+for i in range(1,row+1):
+    for j in range(:wq
+    print(
+for i in range(1,row+1):
+    for j in range(i):
+        print(num,end=" ")
+        num+=2
+    print()
